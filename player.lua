e350f923fd933fbad10c363ba659175d72479b14
yoptohlejepeta <peta.kotlan@seznam.cz>
2022-05-02 10:05:02 +0200

    basic level

--- player.lua
+++ player.lua
@@ -1,24 +1,24 @@
 Player = {}
 
 function Player:load()
-    player = world:newRectangleCollider(width/2, 200, 50,70)
+    player = world:newRectangleCollider(width/2, height -70, 50,70)
     player:setFixedRotation(true)
     --player:applyAngularImpulse(2500)
-    
+    player:setLinearDamping(1,5)
+    player:setFriction(0.5)
 end
 
 function Player:update(dt)
     local px, py = player:getLinearVelocity()
-    print(py,canJump)
     if math.abs(py) < 0.5 then
         canJump = true
     else
         canJump = false
     end
-    if love.keyboard.isDown('right') and px > -1000 then
+    if love.keyboard.isDown('right') and px < 1000 then
         player:applyForce(3000,0)
-    elseif love.keyboard.isDown('left') and py < 1000 then
-        player:applyForce(-3000,0)  
+    elseif love.keyboard.isDown('left') and py > -1000 then
+        player:applyForce(-3000,0)
     end
 end
 
@@ -29,6 +29,6 @@ end
 
 function Player:keypressed(key)
     if key == 'up' and canJump then
-        player:applyLinearImpulse(0,-2500)
+        player:applyLinearImpulse(0,-3500)
     end
 end
